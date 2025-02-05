927298c5e90984fa2e1cf545b2d9c536c16e0aa4
marco-gulino <marcogulino98@gmail.com>
2025-02-05 17:19:00 +0100

    [feat] created CRUD routes in posts.js

---
+++ routers/posts.js
@@ -0,0 +1,34 @@
+//Express IMPORT
+const express = require("express");
+const router = express.Router();
+
+//CRUD Routes of posts
+//INDEX
+router.get("/", (req, res) => {
+
+});
+
+//SHOW
+router.get("/", (req, res) => {
+
+});
+
+//STORE
+router.get("/", (req, res) => {
+
+});
+
+//UPDATE
+router.get("/", (req, res) => {
+
+});
+
+//MODIFY
+router.get("/", (req, res) => {
+
+});
+
+//DELETE
+router.get("/", (req, res) => {
+
+});
